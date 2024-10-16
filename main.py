9afa7553ae4896b1fe81ff72740a54cb026d0e27
CooperNazar <ccnazar@loyola.edu>
2024-10-15 20:08:55 -0400

    Code updates -Cooper

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ while choice != SENTINEL:
     # If the user chooses to deposit, prompt them to input how much they wish to deposit
     if choice == 'D':
         deposit_amount = int(input('How much money do you want to deposit? '))
+        # Make sure the user can't input a negative number
+        while deposit_amount < 0:
+            print('Invalid amount.')
         # Add the amount deposited to the balance
         balance += deposit_amount
         # Output new balance
@@ -27,14 +30,19 @@ while choice != SENTINEL:
     # Otherwise if the user chooses to withdraw, prompt them to input how much they wish to withdraw
     elif choice == 'W':
         withdraw_amount = int(input('How much money do you want to withdraw? '))
+        # Make sure the user can't input a negative number
+        while withdraw_amount < 0:
+            print('Invalid amount.')
         # Subtract the withdrawal amount from the balance
         balance -= withdraw_amount
         # Output new balance
         print('Your balance is: ', balance)
-        # If balance is negative, warn the user that they will be charged 5% interest
     # Otherwise if the user chooses to view their balance, output balance
     elif choice == 'V':
         print('Your balance is: ', balance)
     # Otherwise print that the output was invalid.
     else:
         print('Invalid choice.')
+    # Warn the user that they will be charged 5% interest if their balance is negative
+    if balance < 0:
+        print('Negative balance. You will be charged 5% interest!')
